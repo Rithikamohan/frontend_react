ce7a9d0f1f9373d181f64b42bb7aa98ceba02524
Rithikamohan <122177326+Rithikamohan@users.noreply.github.com>
2023-05-22 21:07:45 +0530

    Update userActions.js

--- src/services/user/userActions.js
+++ src/services/user/userActions.js
@@ -1,7 +1,7 @@
 import * as UT from "./userTypes";
 import axios from "axios";
 
-const REGISTER_URL = "https://student-backend-9zpn.onrender.com/register";
+const REGISTER_URL = "https://student-backend-9zpn.onrender.com/rest/register";
 
 export const fetchUsers = () => {
   return (dispatch) => {
